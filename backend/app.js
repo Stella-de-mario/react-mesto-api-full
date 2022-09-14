f3db871946cdceb3ab9418c45290c7ec9dc7b83f
Stella-de-mario <irina17otr@gmail.com>
2022-09-14 14:19:05 +0500

    project 15.1

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
+const cors = require('cors');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { login, createUser } = require('./controllers/users');
 const { validateLogin, validateCreateUser } = require('./utils/constants');
 const auth = require('./middlewares/auth');
@@ -22,6 +24,30 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.json());
 
+const options = {
+  origin: [
+    'http://localhost:3000',
+    'https://locahost:3000',
+    'http://api.mesto.irinavladi.nomoredomains.sbs',
+    'https://api.mesto.irinavladi.nomoredomains.sbs',
+  ],
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
+  credentials: true,
+};
+
+app.use('*', cors(options));
+
+app.use(requestLogger);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post('/signin', validateLogin, login);
 app.post('/signup', validateCreateUser, createUser);
 
@@ -33,7 +59,7 @@ app.use('/users', require('./routes/users'));
 app.use((req, res, next) => {
   next(new NotFoundError('Запрашиваемая страница не найдена'));
 });
-
+app.use(errorLogger);
 app.use(errors());
 app.use(errorHandler);
 
