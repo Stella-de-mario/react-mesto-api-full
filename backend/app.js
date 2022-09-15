db1608ed5fb35ce755dc5a92d597c6dc782285f1
Stella-de-mario <irina17otr@gmail.com>
2022-09-15 16:19:22 +0500

    dotenv

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
-const cors = require('cors');
+const cors = require('./middlewares/cors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { login, createUser } = require('./controllers/users');
 const { validateLogin, validateCreateUser } = require('./utils/constants');
@@ -24,21 +25,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.json());
 
-const options = {
-  origin: [
-    'http://localhost:3000',
-    'https://locahost:3000',
-    'http://api.mesto.irinavladi.nomoredomains.sbs',
-    'https://api.mesto.irinavladi.nomoredomains.sbs',
-  ],
-  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-  preflightContinue: false,
-  optionsSuccessStatus: 204,
-  allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
-  credentials: true,
-};
-
-app.use('*', cors(options));
+app.use(cors);
 
 app.use(requestLogger);
 
--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -2,12 +2,13 @@ require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../utils/errors/UnauthorizedError');
 
+const { NODE_ENV, JWT_SECRET } = process.env;
 // eslint-disable-next-line consistent-return
 const auth = (req, res, next) => {
   const token = req.cookies.jwt;
   let payload;
   try {
-    payload = jwt.verify(token, 'SECRET');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'secret-key');
   } catch (err) {
     return next(new UnauthorizedError('Пожалуйста, авторизуйтесь'));
   }
