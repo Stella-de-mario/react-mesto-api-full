45094352820938ba44ff0623caeefb8478dbceb2
Stella-de-mario <irina17otr@gmail.com>
2022-09-29 00:46:36 +0500

    project 5

---
+++ frontend/src/utils/Api.js
@@ -0,0 +1,102 @@
+class Api {
+  constructor(options) {
+    this._options = options;
+  }
+
+  _getResponse(res) {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Ошибка: ${res.status}`);
+  }
+
+  getUserInfo() {
+    return fetch(`${this._baseUrl}/users/me`, {
+      method: "GET",
+      headers: this._headers,
+      credentials: 'include'
+    }).then(this._getResponse);
+  }
+
+  getCards() {
+    return fetch(`${this._baseUrl}/cards`, {
+      method: "GET",
+      headers: this._headers,
+      credentials: 'include',
+    }).then(this._getResponse);
+  }
+
+  getData() {
+    return Promise.all([this.getUserInfo(), this.getCards()])
+  }
+
+  setNewUserInfo(data) {
+    return fetch(`${this._baseUrl}/users/me`, {
+      method: "PATCH",
+      headers: this._headers,
+      credentials: 'include',
+      body: JSON.stringify({
+        name: data.name,
+        about: data.about,
+      }),
+    }).then(this._getResponse);
+  }
+
+  setUserAvatar(link) {
+    return fetch(`${this._baseUrl}/users/me/avatar`, {
+      method: "PATCH",
+      headers: this._headers,
+      credentials: 'include',
+      body: JSON.stringify({
+        avatar: link.avatar,
+      }),
+    }).then(this._getResponse);
+  }
+
+  setNewCardsInfo(card) {
+    return fetch(`${this._baseUrl}/cards`, {
+      method: "POST",
+      headers: this._headers,
+      credentials: 'include',
+      body: JSON.stringify({
+        name: card.title,
+        link: card.link,
+      }),
+    }).then(this._getResponse);
+  }
+
+  deleteCard(cardId) {
+    return fetch(`${this._baseUrl}/cards/${cardId}`, {
+      method: "DELETE",
+      headers: this._headers,
+      credentials: 'include'
+    }).then(this._getResponse);
+  }
+
+  changeLikeCardStatus(cardId, isLiked) {
+    if (isLiked) {
+      return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
+        method: "PUT",
+        headers: this._headers,
+        credentials: 'include'
+      }).then(this._getResponse);
+    } else {
+      return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
+        method: "DELETE",
+        headers: this._headers,
+        credentials: 'include'
+      }).then(this._getResponse);
+    }
+  }
+}
+
+const api = new Api({
+  baseUrl: 'https://mesto.irinavladi.nomoredomains.sbs',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  credentials: 'include',
+});
+
+
+export default api;
---
+++ frontend/src/utils/Auth.js
@@ -0,0 +1,48 @@
+class Auth {
+  constructor(options) {
+    this._baseUrl = options.baseUrl;
+  }
+  _headers = {
+    "Content-Type": "application/json",
+  };
+
+  _checkResponse(res) {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Ошибка: ${res.status}`);
+  }
+
+  register({ email, password }) {
+      return fetch(`${this._baseUrl}/signup`, {
+        method: "POST",
+        headers: this._headers,
+        body: JSON.stringify({ email, password }),
+      }).then((res) => this._checkResponse(res));;
+    }
+    
+    authorize({ email, password }) {
+      return fetch(`${this._baseUrl}/signin`, {
+        method: "POST",
+        headers: this._headers,
+        body: JSON.stringify({ email, password }),
+      }).then((res) => this._checkResponse(res));
+    }
+    
+    checkToken(token) {
+      return fetch(`${this._baseUrl}/users/me`, {
+        method: "GET",
+        headers: {
+          ...this._headers,
+          authorization: `Bearer ${token}`,
+        },
+      }).then((res) => this._checkResponse(res));
+    }
+}
+    
+const auth = new Auth({
+  baseUrl: "https://mesto.irinavladi.nomoredomains.sbs",
+  credentials: 'include',
+});
+
+export default auth;
