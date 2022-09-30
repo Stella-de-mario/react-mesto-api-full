4ec5f9e9b1f3226d849d8cbd3d699c9af8f67709
Stella-de-mario <irina17otr@gmail.com>
2022-09-30 22:38:36 +0500

    15.6

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -31,8 +31,7 @@ module.exports.login = (req, res, next) => {
           res.cookie('jwt', token, {
             maxAge: 3600000 * 24 * 7,
             httpOnly: true,
-            sameSite: 'none',
-            secure: true,
+            sameSite: true,
           });
           res.send({ data: user.toJSON() });
         } else {
