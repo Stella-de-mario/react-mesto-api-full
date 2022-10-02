400eed883528ed8a5d79b4a07b3c9621eb7ca58d
Stella-de-mario <irina17otr@gmail.com>
2022-10-03 00:36:10 +0500

    15.1

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -68,30 +68,62 @@ module.exports.createUser = (req, res, next) => {
   const {
     name, about, avatar, email, password,
   } = req.body;
-  bcrypt
-    .hash(password, 10)
-    .then((hashedPassword) => {
-      User.create({
-        name,
-        about,
-        avatar,
-        email,
-        password: hashedPassword,
-      })
-        .then((user) => res.send(user))
-        .catch((err) => {
-          if (err.code === 11000) {
-            next(
-              new ConflictingRequestError('Пользователь с таким email уже зарегистрирован'),
-            );
-          } else if (err.name === 'ValidationError') {
-            next(new BadRequestError('Переданы некорректные данные'));
-          } else {
-            next(err);
-          }
-        });
-    });
+  User.findOne({ email })
+    .then((user) => {
+      if (user) {
+        next(new ConflictingRequestError('Пользователь с таким email уже зарегистрирован'));
+      } else {
+        bcrypt
+          .hash(password, 10)
+          .then((hashedPassword) => {
+            User.create({
+              name,
+              about,
+              avatar,
+              email,
+              password: hashedPassword,
+            })
+              // eslint-disable-next-line no-shadow
+              .then((user) => res.send(user))
+              .catch((err) => {
+                if (err.name === 'ValidationError') {
+                  next(new BadRequestError('Переданы некорректные данные'));
+                }
+              })
+              .catch(next);
+          });
+      }
+    })
+    .catch(next);
 };
+// module.exports.createUser = (req, res, next) => {
+//   const {
+//     name, about, avatar, email, password,
+//   } = req.body;
+//   bcrypt
+//     .hash(password, 10)
+//     .then((hashedPassword) => {
+//       User.create({
+//         name,
+//         about,
+//         avatar,
+//         email,
+//         password: hashedPassword,
+//       })
+//         .then((user) => res.send(user))
+//         .catch((err) => {
+//           if (err.code === 11000) {
+//             next(
+//               new ConflictingRequestError('Пользователь с таким email уже зарегистрирован'),
+//             );
+//           } else if (err.name === 'ValidationError') {
+//             next(new BadRequestError('Переданы некорректные данные'));
+//           } else {
+//             next(err);
+//           }
+//         });
+//     });
+// };
 
 module.exports.getUserId = (req, res, next) => {
   User.findById(req.params.userId)
