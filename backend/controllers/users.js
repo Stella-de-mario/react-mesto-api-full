9f678e40f67187d32a2fb6829d353c977e0d5969
Stella-de-mario <irina17otr@gmail.com>
2022-10-05 00:40:56 +0500

    a

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -42,16 +42,8 @@ module.exports.login = (req, res, next) => {
     .catch(next);
 };
 
-module.exports.logOut = async (req, res, next) => {
-  try {
-    if (!req.cookies) {
-      return next(new NotFoundError('Пользователь с указанным id не найден'));
-    }
-    res.clearCookie('jwt').send().end();
-  } catch (err) {
-    return next(new InternalServerError('Произошла ошибка на сервере'));
-  }
-};
+module.exports.logOut = async (req, res) => res.status(200).clearCookie('jwt').send({});
+
 
 module.exports.getUsers = (req, res, next) => {
   User.find({})
